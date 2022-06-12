b2d0dce92cd8973bfda1e622aaa0abc65f07937a
Sushant Sakhalkar <sushant.sakhalkar@gmail.com>
2022-06-11 18:30:34 -0700

    Create OneDArray.java

---
+++ OneDArray.java
@@ -0,0 +1,10 @@
+//1-D Array <Java>
+/*
+
+An array is a simple data structure used to store a collection of data in a contiguous block of memory. Each element in the collection is accessed using an index, and the elements are easy to find because they're stored sequentially in memory.
+In computer programming, an array is a collection of similar types of data. For example, if we want to store the marks of 100 people then we can create an array of the integer type that can store 100 marks.
+
+int[] array = new int[100];
+
+
+*/
